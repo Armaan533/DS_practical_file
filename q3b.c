db2469f88c9ae85307c4e55e34faf935a90a5d16
Armaan <77953840+Armaan533@users.noreply.github.com>
2023-09-29 14:10:43 +0530

    Updated q3b.c
    Infix to postfix program updated

--- q3b.c
+++ q3b.c
@@ -1,48 +1,79 @@
 #include <stdio.h>
+#include <string.h>
+
+void push(char *stack, int *top, char item)
+{
+	(*top)++;
+	stack[*top] = item;
+}
+
+char pop(char *stack, int *top)
+{
+	*top = *top - 1;
+	return stack[*top+1];
+}
 
 int priority(char c)
 {
-    if(c == '^')
-        return 3;
-    if(c == '*' || c == '/' || c == '%')
-        return 2;
-    if(c == '+' || c == '-')
-        return 1;
-}
-
-void push(int *stack, int *front, int item)
-{
-    if(*front == -1)
-    {
-        *front = 0;
-        stack[*front] = item;
-    }
-    else
-        stack[++(*front)] = item;
-}
-
-int pop(int *stack, int *front)
-{
-    int temp;
-    // if(*front == -1)
-    // {
-    //     printf("\nStack Underflow!\n");
-    //     return;
-    // }
-    temp = stack[*front];
-    (*front)--;
-    return temp;
+	if(c == '+' || c == '-')
+		return 1;
+	if(c == '*' || c == '/')
+		return 2;
+	if(c == '%')
+		return 3;
+	if(c == '^')
+		return 4;
+	if(c == '(')
+		return 5;
+	if(c != ')')
+		return 0;
 }
 
 int main()
 {
-    int i = 0;
-    char infix[50], postfix[50], stack[50], front = -1;
-    printf("Enter the infix expression:\n");
-    fgets(infix, sizeof infix, stdin);
-    while(infix[i] != '\0')
-    {
-        
-        i++
-    }
+	char stack[50], infix[50], expres[50];
+	int i = 0, top = 0, j = 0;
+	printf("Enter the infix expression:\n");
+	fgets(infix, sizeof infix, stdin);
+	infix[strlen(infix)-1] = ')';
+	stack[0] = '(';
+	while(infix[i] != '\0')
+	{
+		if(priority(infix[i]) == 0) 		// Operands will get sent into the expression
+		{
+			expres[j] = infix[i];
+			j++;
+		}
+
+		else if(infix[i] == ')')                                 // for closing parenthesis
+		{
+			while(stack[top] != '(')
+			{
+				expres[j] = pop(stack, &top);
+				j++;
+			}
+			pop(stack, &top);
+		}
+
+		else if(priority(infix[i]) <= priority(stack[top]) && stack[top] != '(')     // for less priority operators
+		{
+			while(priority(stack[top]) >= priority(infix[i]) && stack[top] != '(')
+			{
+				expres[j] = pop(stack, &top);
+				j++;
+			}
+			push(stack, &top, infix[i]);
+		}
+
+		else if(priority(infix[i]) > priority(stack[top]) || stack[top] == '(')       // for high priority operators and opening parenthesis
+			push(stack, &top, infix[i]);
+
+		i++;
+	}
+	expres[j] = '\0';
+
+	printf("Postfix expression:\n");
+	puts(expres);
+	return 0;
+
 }
